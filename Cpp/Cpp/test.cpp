8ca0de0812f74641ef5468cc716e41504a07bd95
dereckdemezquita <dereck@demezquita.com>
2021-12-09 00:10:18 -0500

    Day one data.

---
+++ Cpp/Cpp/test.cpp
@@ -0,0 +1,14 @@
+//
+//  test.cpp
+//  Cpp
+//
+//  Created by Work on 12/8/21.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main () {
+    std::cout << "Hello" << std::endl;
+    return 0;
+}

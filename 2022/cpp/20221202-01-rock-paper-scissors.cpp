cdfde3d6409f887c19ee404f2271ff5c07e58c01
dereckmezquita <dereck@mezquita.io>
2023-10-27 22:44:04 -0500

    New version.

--- 2022/cpp/20221202-01-rock-paper-scissors.cpp
+++ 2022/cpp/20221202-01-rock-paper-scissors.cpp
@@ -5,6 +5,7 @@
 #include <numeric>
 #include <chrono>
 #include <map>
+#include <sstream>
 
 std::string file = "../../data/20221202-rock-paper-scissors.txt";
 
@@ -23,6 +24,19 @@ int resultMatrix[3][3] = {
     {1, 0, -1},
     {-1, 1, 0}
 };
+
+std::string getHumanReadable(int move) {
+    switch (move) {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
+        default:
+            return "INVALID";
+    }
+}
 
 int getScore(int move1, int move2) {
     return resultMatrix[move1][move2];
@@ -47,9 +61,12 @@ int version1() {
 
             int v1 = mapping[p1];
             int v2 = mapping[p2];
-            int result = getScore(v1, v2);
+            int result = getScore(v2, v1);
             totalScore += result;
-            std::cout << "move1Enum: " << result << std::endl;
+            std::cout <<
+                "Opp: " << getHumanReadable(v1) << std::endl <<
+                "Me:  " << getHumanReadable(v2) << std::endl <<
+                "Result: " << result << std::endl << std::endl;
         }
     }
 
@@ -59,9 +76,14 @@ int version1() {
 }
 
 template <typename Func>
-void benchmark(Func func, int num_runs) {
-    std::chrono::duration<double> total_time(0.0);
+void benchmark(Func func, int num_runs, bool silent = false) {
+    std::streambuf* orig_cout = std::cout.rdbuf();
+    std::ostringstream dev_null;
+    if (silent) {
+        std::cout.rdbuf(dev_null.rdbuf());
+    }
 
+    std::chrono::duration<double> total_time(0.0);
     for (int j = 0; j < num_runs; ++j) {
         auto start_time = std::chrono::high_resolution_clock::now();
         func();
@@ -69,9 +91,12 @@ void benchmark(Func func, int num_runs) {
         total_time += end_time - start_time;
     }
 
+    std::cout.rdbuf(orig_cout);
+
     double average_time = total_time.count() / num_runs;
     std::cout << "Function took an average of: " << average_time << " seconds" << std::endl;
 }
+
 
 int main() {
     int num_runs = 20;

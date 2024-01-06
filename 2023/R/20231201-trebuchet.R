0038329ab50f4e7c07f1fc958ea604646518ab3f
dereckmezquita <dereck@mezquita.io>
2024-01-06 17:58:46 -0600

    Optimised and data.table solution.

--- 2023/R/20231201-trebuchet.R
+++ 2023/R/20231201-trebuchet.R
@@ -1,3 +1,25 @@
+box::use(data.table)
+
+dt <- data.table$fread("./2023/data/20231201-trebuchet.txt", header = FALSE)
+
+getNum <- function(val) {
+    indices <- gregexpr("[0-9]", val)
+
+    return(sapply(seq_along(indices), function(i) {
+        idx <- indices[[i]]
+        cur_val <- val[i]
+
+        bol <- substr(cur_val, idx, idx)
+        eol <- substr(cur_val, idx[length(idx)], idx[length(idx)])
+        return(as.numeric(paste0(bol, eol)))
+    }))
+}
+
+main <- function() {
+    dt[, score := getNum(V1)]
+    sum(dt$score)   
+}
+# ----
 con <- file("./2023/data/20231201-trebuchet.txt", "r")
 
 lines <- readLines(con)
@@ -5,16 +27,16 @@ close(con)
 
 process_lines <- function(lines) {
     # Extract numbers and convert to numeric
-    numbers <- as.numeric(gsub("[^0-9]", "", lines))
+    numbers <- gsub("[a-z]", "", lines)
 
     # get the first and last digit of each number
-    first_digit <- floor(numbers / 10 ^ (nchar(numbers) - 1))
-    last_digit <- numbers %% 10
+    first_digit <- substr(numbers, 1, 1)
+    last_digit <- substr(numbers, nchar(numbers), nchar(numbers))
 
-    num <- paste0(first_digit, last_digit)
+    num <- as.numeric(paste0(first_digit, last_digit))
     
     # Process and sum the numbers
-    print(sum(as.numeric(num)))
+    print(sum(num))
 }
 
 options(scipen = 999)
